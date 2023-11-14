e5ae388658d425ec6eaba9cd2f550600a0994c43
wansawangkanchanakaree <115473355+wansawangkanchanakaree@users.noreply.github.com>
2023-11-14 21:58:12 +0700

    Create foot.js

---
+++ js/foot.js
@@ -0,0 +1,13 @@
+// SweetAlert2
+window.Swal = require('sweetalert2');
+
+// Popper.js
+require('@popperjs/core/dist/umd/popper.min.js');
+
+// Bootstrap
+require('bootstrap/dist/js/bootstrap.bundle.min.js');
+
+// DataTable
+require('jquery.dataTables.min.js');
+require('dataTables.responsive.min.js');
+require('dataTables.buttons.min.js');
