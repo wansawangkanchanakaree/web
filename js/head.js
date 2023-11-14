02637d5d14aba0d3e61b60b9ed5a05908ce671db
wansawangkanchanakaree <115473355+wansawangkanchanakaree@users.noreply.github.com>
2023-11-14 21:26:30 +0700

    Create head.js

---
+++ js/head.js
@@ -0,0 +1,6 @@
+var html =  '<script src="https://code.jquery.com/jquery-3.7.0.js"></script>'+
+'  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />'+
+'  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">'+
+'  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" />'+
+'  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.dataTables.min.css" />'+
+'  <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css" />';
